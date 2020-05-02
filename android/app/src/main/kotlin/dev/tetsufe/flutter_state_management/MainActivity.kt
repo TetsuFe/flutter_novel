08b65321abf17cb4543082c8bd2528859c2a72a1
TetsuFe <tetsubeg1ner27@gmail.com>
2020-05-03 01:52:26 +0900

    init

---
+++ android/app/src/main/kotlin/dev/tetsufe/flutter_state_management/MainActivity.kt
@@ -0,0 +1,12 @@
+package dev.tetsufe.flutter_state_management
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
