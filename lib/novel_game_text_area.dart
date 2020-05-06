c0f7f7b7172b0bb3bf897e2ed4740d1bcaee6f82
TetsuFe <tetsubeg1ner27@gmail.com>
2020-05-06 10:53:09 +0900

    Update novel game page ui

--- lib/novel_game_text_area.dart
+++ lib/novel_game_text_area.dart
@@ -6,20 +6,33 @@ import 'package:provider/provider.dart';
 class NovelGageTextArea extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text(
-          context.select<SentenceState, String>((s) => s.currentSentence),
-        ),
-        FlatButton(
-          child: Text(
-            '次へ',
-          ),
-          onPressed: () {
-            context.read<SentenceStateNotifier>().goToNextSentence();
-          },
-        )
-      ],
+    return Center(
+      child: Column(
+        children: [
+          Expanded(
+              flex: 7,
+              child: Image.network(
+                'https://userdisk.webry.biglobe.ne.jp/012/472/52/N000/000/000/BG26a_80.jpg',
+                fit: BoxFit.contain,
+              )),
+          Expanded(
+              flex: 3,
+              child: GestureDetector(
+                  onTap: () {
+                    context.read<SentenceStateNotifier>().goToNextSentence();
+                  },
+                  child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 64),
+                      child: Container(
+                          width: double.infinity,
+                          color: Colors.green[400],
+                          child: Text(
+                            context.select<SentenceState, String>(
+                                (s) => s.currentSentence),
+                            style: TextStyle(color: Colors.white, fontSize: 16),
+                          )))))
+        ],
+      ),
     );
   }
 }
