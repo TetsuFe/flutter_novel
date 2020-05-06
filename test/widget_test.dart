b215fa16b275ab1a415ab5615756b6b56591b0e1
TetsuFe <tetsubeg1ner27@gmail.com>
2020-05-06 09:12:21 +0900

    Refactor test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,20 +1,10 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
-// import 'package:flutter_state_management/story_details_page.dart';
 import 'package:flutter_state_management/story_details_body.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:image_test_utils/image_test_utils.dart';
 import 'package:flutter_state_notifier/flutter_state_notifier.dart';
 import 'package:flutter_state_management/story_state_notifier.dart';
 import 'package:flutter_state_management/story.dart';
-
-// class MockNavigationObserver extends Mock implements Navigator{};
 
 void main() {
   testWidgets('marks as read a story when open novel game page',
