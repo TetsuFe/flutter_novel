e536fbbc899c87778ebff87a21b3425c5cbeb347
TetsuFe <tetsubeg1ner27@gmail.com>
2020-05-06 18:09:03 +0900

    Move github link place

--- lib/story/story_details_page.dart
+++ lib/story/story_details_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_state_management/story/story_details_body.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class StoryDetailsPage extends StatelessWidget {
   @override
@@ -21,18 +20,6 @@ class StoryDetailsPage extends StatelessWidget {
               Center(
                 child: StoryDetailsBody(),
               ),
-              GestureDetector(
-                onTap: () {
-                  launch('https://github.com/tetsufe/state_notifier_sample6/');
-                },
-                child: Text(
-                  'GitHubリポジトリはこちら',
-                  style: TextStyle(
-                    decoration: TextDecoration.underline,
-                    color: Colors.blue,
-                  ),
-                ),
-              )
             ],
           ),
         ),
--- lib/story_list_page.dart
+++ lib/story_list_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter_state_management/story/story_details_page.dart';
 import 'package:flutter_state_management/story/story_state_notifier.dart';
 import 'package:flutter_state_notifier/flutter_state_notifier.dart';
 import 'package:provider/provider.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class StoryListPage extends StatelessWidget {
   @override
@@ -13,34 +14,53 @@ class StoryListPage extends StatelessWidget {
         title: Text('ストーリー選択'),
       ),
       body: SingleChildScrollView(
-        child: ListView.builder(
-          shrinkWrap: true,
-          itemCount: 1,
-          primary: true,
-          itemBuilder: (context, index) => ListTile(
-              title: Text('chapter1'),
-              onTap: () {
-                final story = Story(
-                    id: 1,
-                    title: 'chapter1',
-                    summary: 'テストストーリーです',
-                    thumbnailImagePath: 'assets/story/1/hokuma.jpg',
-                    isRead: false);
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return MultiProvider(
-                        providers: [
-                          StateNotifierProvider<StoryStateNotifier, Story>(
-                            create: (_) => StoryStateNotifier(story),
-                          )
-                        ],
-                        child: StoryDetailsPage(),
-                      );
-                    },
+        child: Column(
+          children: <Widget>[
+            ListView.builder(
+              shrinkWrap: true,
+              itemCount: 1,
+              primary: true,
+              itemBuilder: (context, index) => ListTile(
+                  title: Text('chapter1'),
+                  onTap: () {
+                    final story = Story(
+                        id: 1,
+                        title: 'chapter1',
+                        summary: 'テストストーリーです',
+                        thumbnailImagePath: 'assets/story/1/hokuma.jpg',
+                        isRead: false);
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return MultiProvider(
+                            providers: [
+                              StateNotifierProvider<StoryStateNotifier, Story>(
+                                create: (_) => StoryStateNotifier(story),
+                              )
+                            ],
+                            child: StoryDetailsPage(),
+                          );
+                        },
+                      ),
+                    );
+                  }),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 32),
+              child: GestureDetector(
+                onTap: () {
+                  launch('https://github.com/tetsufe/state_notifier_sample6/');
+                },
+                child: Text(
+                  'GitHubリポジトリはこちら',
+                  style: TextStyle(
+                    decoration: TextDecoration.underline,
+                    color: Colors.blue,
                   ),
-                );
-              }),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
