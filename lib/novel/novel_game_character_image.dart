ce5db21939a160ac346691cdb933152b94ad540e
TetsuFe <tetsubeg1ner27@gmail.com>
2020-05-30 17:57:07 +0900

    Implement zoom effect

--- lib/novel/novel_game_character_image.dart
+++ lib/novel/novel_game_character_image.dart
@@ -17,30 +17,95 @@ class NovelGameCharacterImage extends StatelessWidget {
             width: double.infinity,
             height: double.infinity,
             child: VTweetAnimationContainer(
-              child: CharacterImageContainer(
-                  charecterImagePath: currentCharecterImagePath),
+              child: ZoomedCharacterImageContainer(
+                charecterImagePath: currentCharecterImagePath,
+                zoomLevel: context.select<SentenceState, String>(
+                    (s) => s.currentCharecterImageEffect),
+              ),
             ),
           );
         }
-        return CharacterImageContainer(
-            charecterImagePath: currentCharecterImagePath);
+        return ZoomedCharacterImageContainer(
+          charecterImagePath: currentCharecterImagePath,
+          zoomLevel: context.select<SentenceState, String>(
+              (s) => s.currentCharecterImageEffect),
+        );
       },
     ));
   }
 }
 
-class CharacterImageContainer extends StatelessWidget {
+mixin AdaptiveSizeWidget {
+  double culcurateContainerWidth(BuildContext context);
+  double culcurateContainerHeight(BuildContext context);
+}
+
+class CharacterImageContainer extends StatelessWidget
+    implements AdaptiveSizeWidget {
   const CharacterImageContainer({@required this.charecterImagePath});
   final String charecterImagePath;
 
   @override
-  Widget build(BuildContext context) {
+  double culcurateContainerWidth(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
+    return screenWidth / 2;
+  }
+
+  @override
+  double culcurateContainerHeight(BuildContext context) {
     final screenHeight = MediaQuery.of(context).size.height;
+    return screenHeight / 2;
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
-      width: screenWidth / 2,
-      height: screenHeight / 2,
-      child: Image.network(charecterImagePath),
+      width: culcurateContainerWidth(context),
+      height: culcurateContainerHeight(context),
+      child: Image.network(
+        charecterImagePath,
+        fit: BoxFit.fill,
+      ),
+    );
+  }
+}
+
+class ZoomedCharacterImageContainer extends StatelessWidget
+    with AdaptiveSizeWidget {
+  const ZoomedCharacterImageContainer(
+      {@required this.charecterImagePath, @required this.zoomLevel});
+  final String charecterImagePath;
+  final String zoomLevel;
+
+  double zoomLevelAsDouble() {
+    try {
+      return double.parse(zoomLevel);
+    } on Exception catch (_) {
+      return 1;
+    }
+  }
+
+  @override
+  double culcurateContainerWidth(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    return screenWidth / 2 * zoomLevelAsDouble();
+  }
+
+  @override
+  double culcurateContainerHeight(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    return screenHeight / 2 * zoomLevelAsDouble();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: culcurateContainerWidth(context),
+      height: culcurateContainerHeight(context),
+      child: Image.network(
+        charecterImagePath,
+        fit: BoxFit.contain,
+      ),
     );
   }
 }
