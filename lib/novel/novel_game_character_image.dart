aed58091e60faceaccfdfddf023e3b79cb012132
TetsuFe <tetsubeg1ner27@gmail.com>
2020-05-30 17:01:56 +0900

    Refactor: Extract character image container

--- lib/novel/novel_game_character_image.dart
+++ lib/novel/novel_game_character_image.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_state_management/novel/v_tween_animation_container.dart';
 import 'package:flutter_state_management/sentence/sentence_state.dart';
+import 'package:meta/meta.dart';
 import 'package:provider/provider.dart';
 
 class NovelGameCharacterImage extends StatelessWidget {
@@ -13,21 +14,31 @@ class NovelGameCharacterImage extends StatelessWidget {
         if (currentCharecterImagePath ==
             'assets/character_images/ypose_hokuma.png') {
           return Container(
-              width: double.infinity,
-              height: double.infinity,
-              child: VTweetAnimationContainer(
-                  child: Container(
-                      width:
-                          MediaQuery.of(context).size.height > 600 ? 400 : 240,
-                      height:
-                          MediaQuery.of(context).size.height > 600 ? 400 : 240,
-                      child: Image.network(currentCharecterImagePath))));
+            width: double.infinity,
+            height: double.infinity,
+            child: VTweetAnimationContainer(
+              child: CharacterImageContainer(
+                  charecterImagePath: currentCharecterImagePath),
+            ),
+          );
         }
-        return Container(
-            width: MediaQuery.of(context).size.height > 600 ? 400 : 240,
-            height: MediaQuery.of(context).size.height > 600 ? 400 : 240,
-            child: Image.network(currentCharecterImagePath));
+        return CharacterImageContainer(
+            charecterImagePath: currentCharecterImagePath);
       },
     ));
+  }
+}
+
+class CharacterImageContainer extends StatelessWidget {
+  const CharacterImageContainer({@required this.charecterImagePath});
+  final String charecterImagePath;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.height > 600 ? 400 : 240,
+      height: MediaQuery.of(context).size.height > 600 ? 400 : 240,
+      child: Image.network(charecterImagePath),
+    );
   }
 }
