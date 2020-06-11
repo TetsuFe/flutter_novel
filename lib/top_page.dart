30abfbb1a708c99f52432e86a6825ddd5cd93e77
TetsuFe <tetsubeg1ner27@gmail.com>
2020-06-11 20:28:06 +0900

    Rename StoryListPage to StoryList

--- lib/top_page.dart
+++ lib/top_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_state_management/story_list_page.dart';
+import 'package:flutter_state_management/story/story_list.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class TopPage extends StatelessWidget {
